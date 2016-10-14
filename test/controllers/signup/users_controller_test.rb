13e0215a107dae2af5e4b7b33425ce1333b83e2e
hmtanbir <tanbir2025@gmail.com>
2016-10-14 22:40:30 +0600

    test updated

--- test/controllers/signup/users_controller_test.rb
+++ test/controllers/signup/users_controller_test.rb
@@ -4,49 +4,65 @@ module Signup
   class UsersControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
-    setup do
-      @user = signup_users(:one)
+    def setup
+      @user = users(:one)
+      @other_user = users(:two)
     end
 
-    test "should get index" do
-      get users_url
+    test 'should redirect index when not logged in' do
+      get :index
+      assert_redirected_to login_url
+    end
+
+    test 'should get new when logged in admin user' do
+      log_in_as(@user)
+      get :new
       assert_response :success
     end
 
-    test "should get new" do
-      get new_user_url
-      assert_response :success
-    end
-
-    test "should create user" do
-      assert_difference('User.count') do
-        post users_url, params: { user: { admin: @user.admin, email: @user.email, firstname: @user.firstname, lastname: @user.lastname, password_digest: @user.password_digest, remember_digest: @user.remember_digest } }
+    test 'should get new when logged in other user' do
+      log_in_as(@other_user)
+      assert_no_difference 'User.count' do
+        get :new
       end
-
-      assert_redirected_to user_url(User.last)
+      assert_redirected_to admin_url
     end
 
-    test "should show user" do
-      get user_url(@user)
-      assert_response :success
+    test 'should redirect edit when not logged in' do
+      get :edit, id: @user
+      assert_redirected_to login_url
     end
 
-    test "should get edit" do
-      get edit_user_url(@user)
-      assert_response :success
+    test 'should redirect update when not logged in' do
+      patch :update, id: @user, user: {name: @user.name, email: @user.email}
+      assert_redirected_to login_url
     end
 
-    test "should update user" do
-      patch user_url(@user), params: { user: { admin: @user.admin, email: @user.email, firstname: @user.firstname, lastname: @user.lastname, password_digest: @user.password_digest, remember_digest: @user.remember_digest } }
-      assert_redirected_to user_url(@user)
+    test 'should redirect edit when logged in as wrong user' do
+      log_in_as(@other_user)
+      get :edit, id: @user
+      assert_redirected_to admin_url
     end
 
-    test "should destroy user" do
-      assert_difference('User.count', -1) do
-        delete user_url(@user)
+    test 'should redirect update when logged in as wrong user' do
+      log_in_as(@other_user)
+      patch :update, id: @user, user: {name: @user.name, email: @user.email}
+      assert_redirected_to admin_url
+    end
+
+    test 'should redirect destroy when not logged in' do
+      assert_no_difference 'User.count' do
+        delete :destroy, id: @user
       end
-
-      assert_redirected_to users_url
+      assert_redirected_to login_url
     end
-  end
+
+    test 'should redirect destroy when logged in as a non-admin' do
+      log_in_as(@other_user)
+      assert_no_difference 'User.count' do
+        delete :destroy, id: @user
+      end
+      assert_redirected_to admin_url
+    end
+   end
 end
