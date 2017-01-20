023a59b5423d5bde56a52d83a00bec75a25187a1
hmtanbir <tanbir2025@gmail.com>
2017-01-20 16:49:21 +0600

    signup.scss removed

--- lib/signup/engine.rb
+++ lib/signup/engine.rb
@@ -7,10 +7,6 @@ module Signup
       g.assets false
     end
 
-    initializer 'signup.assets.precomple' do |app|
-      app.config.assets.precompile += %w( signup.scss )
-    end
-
     initializer :append_migrations do |app|
       unless app.root.to_s.match root.to_s
         config.paths['db/migrate'].expanded.each do |expanded_path|
